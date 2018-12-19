d58f77930bffe6e0e81f7adec5efd72633690143
rafad209 <38476918+rafad209@users.noreply.github.com>
2018-12-18 17:53:36 -0800

    Add files via upload

--- CS4250/League.php
+++ CS4250/League.php
@@ -14,57 +14,29 @@
 
 
 <?php
-
 $username = "aperez";
 $password = "wcz96PPF";
 $database = "aperez";
 $server = "hopper.csustan.edu";
-
-
-
-
-
-
-
 	try {
-
-
 		$conn = new PDO("mysql:hodst=$server;dbname=$database", $username, $password);
-
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         $query = $conn->prepare("SELECT * FROM League");
         $query->execute();
+        echo "<table class='container striped centered'>";
+        echo "<tr><th>Nat. League ID</th><th>League Name</th></tr>";
 
-        echo "<table class='container'>";
+        
         for ($i = 0; $row = $query->fetch(); $i++) {
-
             echo "<tr><td>" . $row['natleagueid'] . "</td>";
             echo "<td>" . $row['leaguename'] . "</td></tr>";
-
-
-
         }
-
         echo "</table>";
-
     }
 catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
-
-
-
-
-
-
-
-
-
     $conn->null;
-
-
-
 ?>
 
 
