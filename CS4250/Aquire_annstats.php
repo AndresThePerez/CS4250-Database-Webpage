254d9b45efa68a824f250b954f77474a790a63cc
rafad209 <38476918+rafad209@users.noreply.github.com>
2018-12-18 17:00:51 -0800

    Add files via upload

--- CS4250/Aquire_annstats.php
+++ CS4250/Aquire_annstats.php
@@ -14,59 +14,32 @@
 
 
 <?php
-
 $username = "aperez";
 $password = "wcz96PPF";
 $database = "aperez";
 $server = "hopper.csustan.edu";
-
-
-
-
-
-
-
 	try {
-
-
 		$conn = new PDO("mysql:hodst=$server;dbname=$database", $username, $password);
-
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         $query = $conn->prepare("SELECT * FROM Aquire_annstats");
         $query->execute();
-
-         echo "<table class='container'>";
+         echo "<table class='container striped centered'>";
+         echo "<tr><th>Natteamid</th><th>Wins</th><th>Losses</th><th></th>Game_Played</th><th>Year<th></tr>";
+         
+         
         for ($i = 0; $row = $query->fetch(); $i++) {
-
             echo "<tr><td>" . $row['natteamid'] . "</td>";
             echo "<td>" . $row['wins'] . "</td>";
             echo "<td>" . $row['losses'] . "</td>";
             echo "<td>" . $row['game_played'] . "</td>";
             echo "<td>" . $row['year'] . "</td></tr>";
-
-
         }
-
         echo "</table>";
-
     }
 catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
-
-
-
-
-
-
-
-
-
     $conn->null;
-
-
-
 ?>
 
 
